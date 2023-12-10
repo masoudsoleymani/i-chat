2ecb258c20c7fbe19263bd5261d6665ce969dc82
Masoud Soleymani <msoleym@tbdir.net>
2023-12-10 01:16:18 +0000

    feat: create nav component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { getServerSession } from "next-auth";
+
+import SessionProvider from "../components/SessionProvider";
+import NavMenu from "../components/NavMenu";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,14 +13,23 @@ export const metadata: Metadata = {
   description: 'GPT-4 Streaming Chat Application',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await getServerSession();
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>        
+        <SessionProvider session={session}>
+          <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <NavMenu />
+            {children}
+          </main>
+        </SessionProvider>
+      </body>
     </html>
   )
 }
